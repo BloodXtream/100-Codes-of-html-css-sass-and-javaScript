e548012e4bc18676d5084f07169c23920ceb3f94
Pragyanam Tiwari <125180282+BloodXtream@users.noreply.github.com>
2025-01-05 13:26:15 +0530

    Add files via upload

---
+++ emojis/script.js
@@ -0,0 +1,6 @@
+const emoji = document.querySelector("#emoji")
+const emojis = ["😀", "🚀", "🌈", "🍕", "🐱", "⚽", "🎉", "🌟", "💻", "🎵"]
+
+emoji.addEventListener('mouseover',()=>{
+    emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)]
+})
